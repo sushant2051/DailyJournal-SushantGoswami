07cced95ebe03cdc14f09727aca01dde3e8e46a6
sushant2051 <sushantdevloper1994@gmail.com>
2026-01-03 21:10:13 +0545

    Milestone 2 done

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace JournalManagementSystem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+
+namespace JournalManagementSystem
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+            builder.Services.AddMudServices();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
